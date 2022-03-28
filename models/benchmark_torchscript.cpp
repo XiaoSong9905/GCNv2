f7f3157be570be75f53a6090cfb4e95bdc2fa229
Xiao Song <xiaosx@berkeley.edu>
2022-03-27 21:35:19 -0700

    finish torchscript part

--- models/benchmark_torchscript.cpp
+++ models/benchmark_torchscript.cpp
@@ -1,6 +1,6 @@
 //
 // Load and run TorchScript model in cpp 
-// Reference: https://pytorch.org/tutorials/advanced/cpp_export.html#step-3-loading-your-script-module-in-c
+// Reference: https://pytorch.org/tutorials/advanced/cpp_export.html#step-3-loading-your-script-torch_model-in-c
 // 
 #include <torch/script.h>
 #include <opencv2/opencv.hpp>
@@ -17,58 +17,55 @@ int main(int argc, const char* argv[])
         return -1;
     }
 
-    // Load torchscript module
-    torch::jit::script::Module module;
+    // Load torchscript torch_model
+    printf("..Load torchscript model\n");
+    torch::jit::script::Module torch_model;
     try 
     {
-        // Deserialize the ScriptModule from a file using torch::jit::load().
-        module = torch::jit::load(argv[1]);
+        // Deserialize the Module from a file using torch::jit::load().
+        torch_model = torch::jit::load(argv[1]);
     }
-    catch (const c10::Error& e) 
+    catch ( std::exception& e )
     {
-        printf( "error loading the model" );
+        printf("Error loading model. %s\n", e.what() );
         return -1;
     }
 
     // Initialize random data in cv::Mat format
-    cv::Mat input_opencv = cv::imread( "../sather_gate.jpg", cv::IMREAD_GRAYSCALE );
+    printf("..Read input image\n");
     int img_height = 640;
     int img_width = 480;
-    cv::resize( input_opencv, input_opencv, cv::Size( img_width, img_height ) );
+    cv::Mat input_opencv = cv::imread( "../sather_gate.jpg", cv::IMREAD_GRAYSCALE );
     input_opencv.convertTo( input_opencv, CV_32FC1, 1.0f / 255.0f, 0 );
-
-    int num_iter = 1000;
-    std::vector<cv::Mat> inputs_opencv( num_iter );
-    for ( int i = 0; i < num_iter; i++ ) // every iteration
-    {
-        inputs_opencv.emplace_back( input_opencv.clone() );
-    }
+    cv::resize( input_opencv, input_opencv, cv::Size( img_width, img_height ) );
 
     // Set device
     torch::Device device( torch::DeviceType::CPU );
 
     /* Timming */
     // We timming both convert cv::Mat to corresbonding data format and time to run the model
+    int num_iter = 100;
+
+    printf("..Benchmark for %d iterations\n", num_iter );
     auto start_time = std::chrono::steady_clock::now();
 
     for ( int i = 0; i < num_iter; ++i )
     {
         // Convert cv::Mat input to torchscript input
         std::vector<int64_t> dims = {1, img_height, img_width, 1};
-        auto input_torch = torch::from_blob( inputs_opencv[ i ].data, dims, torch::kFloat32 ).to(device);
+        auto input_torch = torch::from_blob( input_opencv.data, dims, torch::kFloat32 ).to(device);
         input_torch = input_torch.permute({0,3,1,2});
 
         std::vector<torch::jit::IValue> inputs_torch;
         inputs_torch.push_back( input_torch );
 
         // Run model
-        auto outputs_torch = module.forward(inputs_torch).toTuple();
+        auto outputs_torch = torch_model.forward(inputs_torch).toTuple();
 
-        auto pts  = outputs_torch->elements()[0].toTensor().to(torch::kCPU).squeeze();
-        auto desc = outputs_torch->elements()[1].toTensor().to(torch::kCPU).squeeze();
+        torch::Tensor pts  = outputs_torch->elements()[0].toTensor().to(torch::kCPU).squeeze();
+        torch::Tensor desc = outputs_torch->elements()[1].toTensor().to(torch::kCPU).squeeze();
 
-        cv::Mat pts_mat(cv::Size(3, pts.size(0)), CV_32FC1, pts.data_ptr<float>());
-        cv::Mat desc_mat(cv::Size(32, pts.size(0)), CV_8UC1, desc.data_ptr<unsigned char>());
+        // TODO: add convert back to cv::mat
     }
 
     auto end_time = std::chrono::steady_clock::now();
@@ -77,6 +74,6 @@ int main(int argc, const char* argv[])
     std::chrono::duration<double> duration_time = end_time - start_time;
     double duratin_time_in_sec = duration_time.count();
     duratin_time_in_sec /= num_iter;
-    printf("TorchScript make %lf second to run each model inference\n", duratin_time_in_sec );
-    printf("TorchScript model can run %d fps\n", int( 1.0 / duratin_time_in_sec ) );
+    printf("..TorchScript make %lf second to run each model inference\n", duratin_time_in_sec );
+    printf("..TorchScript model can run %d fps\n", int( 1.0 / duratin_time_in_sec ) );
 }
--- models/run_torchscript.cpp
+++ models/run_torchscript.cpp
@@ -18,38 +18,43 @@ int main(int argc, const char* argv[])
     }
 
     // Load torchscript module
-    torch::jit::script::Module module;
+    torch::jit::script::Module model;
     try 
     {
         // Deserialize the ScriptModule from a file using torch::jit::load().
-        printf("Try to load %s\n", argv[1]);
-        module = torch::jit::load(argv[1]);
+        printf("..Try to load %s\n", argv[1]); fflush(stdout);
+        model = torch::jit::load(argv[1]);
     }
-    catch (const c10::Error& e) 
+    catch ( std::exception& e )
     {
-        printf( "error loading the model" );
-        throw std::runtime_error("Unable to load torchscript file\n");
+        printf("Error loading model. %s\n", e.what() );
+        return -1;
     }
 
-    // Set device
     torch::Device device( torch::DeviceType::CPU );
 
     // Set input
-    int img_height = 640;
-    int img_width = 480;
-    std::vector<torch::jit::IValue> inputs_torch;
-    inputs_torch.push_back( torch::ones({1, 1, img_height, img_width}));
+    printf("..Read in input\n"); fflush(stdout);
+    int img_width = 640;
+    int img_height = 480;
+    cv::Mat image = cv::imread( "../sather_gate.jpg", cv::IMREAD_GRAYSCALE );
+    image.convertTo(image, CV_32FC1, 1.f / 255.f , 0);
+    cv::resize(image, image, cv::Size(img_width, img_height));
+
+    // Convert to torchscript format
+    printf("..Convert to torchscript data\n"); fflush(stdout);
+    std::vector<int64_t> dims = {1, img_height, img_width, 1};
+    auto img_var = torch::from_blob( image.data, dims, torch::kFloat32).to(device);
+    img_var = img_var.permute({0,3,1,2});
 
     // Run model
-    auto outputs_torch = module.forward(inputs_torch).toTuple();
+    printf("..Run model\n"); fflush(stdout);
+    std::vector<torch::jit::IValue> inputs;
+    inputs.push_back(img_var);
+    auto output = model.forward(inputs).toTuple();
 
-    auto pts  = outputs_torch->elements()[0].toTensor().to(torch::kCPU).squeeze();
-    auto desc = outputs_torch->elements()[1].toTensor().to(torch::kCPU).squeeze();
-
-    // Convert output to cv::Mat to ensure model acrually run
-    cv::Mat pts_mat(cv::Size(3, pts.size(0)), CV_32FC1, pts.data_ptr<float>());
-    cv::Mat desc_mat(cv::Size(32, pts.size(0)), CV_8UC1, desc.data_ptr<unsigned char>());
-
-    printf("pts_mat size %d %d\n", pts_mat.size().height, pts_mat.size().width );
-    printf("desc_mat size %d %d\n", desc_mat.size().height, desc_mat.size().width );
+    // Extract output
+    printf("..Extract output\n"); fflush( stdout );
+    torch::Tensor pts  = output->elements()[0].toTensor().to(torch::kCPU).squeeze();
+    torch::Tensor desc = output->elements()[1].toTensor().to(torch::kCPU).squeeze();
 }
